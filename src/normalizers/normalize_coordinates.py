7fe6114a48495a6a406707b0b5e4c215543c6387
Todd Jacobus <rtjacobus1@gmail.com>
2020-12-05 15:45:15 -0500

    finished normalizing x coordinates

--- src/normalizers/normalize_coordinates.py
+++ src/normalizers/normalize_coordinates.py
@@ -1,4 +1,13 @@
-
+from src.validators.validate_coordinates import coordinates_are_valid
 
 def normalize_point(coordinates):
-    return coordinates
+    if not coordinates_are_valid(coordinates):
+        x, y = coordinates
+        
+        num_circles = int(x / 360)
+        degs = num_circles * 360
+        norm_degs = x - degs
+        if norm_degs > 180: norm_degs = norm_degs - 360
+        if norm_degs < -180: norm_degs = norm_degs + 360
+        
+        return norm_degs
