8bd8dcbca4424a5bf0209d8158eca95d608619ab
Todd Jacobus <rtjacobus1@gmail.com>
2020-12-06 06:49:55 -0500

    rough solution finished

--- src/normalizers/normalize_coordinates.py
+++ src/normalizers/normalize_coordinates.py
@@ -4,10 +4,29 @@ def normalize_point(coordinates):
     if not coordinates_are_valid(coordinates):
         x, y = coordinates
         
-        num_circles = int(x / 360)
-        degs = num_circles * 360
-        norm_degs = x - degs
-        if norm_degs > 180: norm_degs = norm_degs - 360
-        if norm_degs < -180: norm_degs = norm_degs + 360
+        num_circles_x = int(x / 360)
+        degs_x = num_circles_x * 360
+        norm_degs_x = x - degs_x
+        if norm_degs_x > 180: norm_degs_x = norm_degs_x - 360
+        if norm_degs_x < -180: norm_degs_x = norm_degs_x + 360
         
-        return norm_degs
+        # num_circles_y = int(y / 90)
+        # degs_y = num_circles_y * 90
+        # norm_degs_y = y - degs_y
+        # if 90 < norm_degs_y <= 270: norm_degs_y = 180 - norm_degs_y
+        # if norm_degs_y > 270: norm_degs_y = 360 - norm_degs_y
+
+        num_circles_y = int(y / 360)
+        degs_y = num_circles_y * 360
+        norm_degs_y = y - degs_y
+        quadrant = int(norm_degs_y / 90)
+        
+        if 90 < norm_degs_y <= 180: norm_degs_y = 180 - norm_degs_y
+        if 180 < norm_degs_y <= 270: norm_degs_y = 180 - norm_degs_y
+        if 270 < norm_degs_y <= 360: norm_degs_y = norm_degs_y - 360
+
+        if -180 < norm_degs_y <= -90: norm_degs_y = -180 - norm_degs_y
+        if -270 < norm_degs_y <= -180: norm_degs_y = -180 - norm_degs_y
+        if -360 < norm_degs_y <= -270: norm_degs_y = norm_degs_y + 360
+
+        return (norm_degs_x, norm_degs_y)
--- tests/test_normalizers.py
+++ tests/test_normalizers.py
@@ -4,7 +4,7 @@ from src.normalizers.normalize_coordinates import normalize_point
 from src.validators.validate_coordinates import coordinates_are_valid
 
 class NormalizerTests(TestCase):
-    def test_normalize_point_text_coords(self):
+    def test_valid_points(self):
         sample_points = {
             "point_A": (190, 240),
             "point_B": (-210, 120),
@@ -19,17 +19,37 @@ class NormalizerTests(TestCase):
             self.assertTrue(
                 coordinates_are_valid(normalized_point)
                 )
-
     
+    def test_correct_points(self):
+        sample_points = {
+            "point_A": {
+                "raw": (190, 240),
+                "norm": (-170, -60),
+                },
+            "point_B": {
+                "raw": (-210, 120),
+                "norm": (150, 60)
+                },
+            "point_C": {
+                "raw": (720, -10),
+                "norm": (0, -10),
+                },
+            "point_D": {
+                "raw": (-750, 0),
+                "norm": (-30, 0),
+                },
+            "point_E": { 
+                "raw": (-890, -100),
+                "norm": (-170, -80)
+                },
+            "point_F": {
+                "raw": (920, 180),
+                "norm": (-160, 0),
+                },
+        }
 
-# normalizePoint(new Point(190, 240))  ->  Point(-170, -60)
-
-# normalizePoint(new Point(-210, 120)) ->  Point(150, 60)
-
-# normalizePoint(new Point(720, -10))  ->  Point(0, -10)
-
-# normalizePoint(new Point(-750, 0)) -> Point(-30, 0)
-
-# normalizePoint(new Point(-890, -100)) -> Point(-170, -80)
-
-# normalizePoint(new Point(920, 180))   -> Point(-160, 0
+        for point, coords in sample_points.items():
+            self.assertEqual(
+                normalize_point(coords['raw']),
+                coords['norm']
+            )
