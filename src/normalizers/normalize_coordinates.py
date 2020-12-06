9e76d102706e12d6af4f70e42a1f58eb70f0cea8
Todd Jacobus <rtjacobus1@gmail.com>
2020-12-06 09:09:54 -0500

    cleaned up code; added comments; added readme

--- src/normalizers/normalize_coordinates.py
+++ src/normalizers/normalize_coordinates.py
@@ -1,16 +1,26 @@
 from src.validators.validate_coordinates import coordinates_are_valid
 
 def normalize_point(coordinates):
+    """Normalization function which converts degrees of a circle to 
+    acceptable values for geographic, latitude and longitude."""
+    
     if not coordinates_are_valid(coordinates):
+        # Check if coordinates are valid.  If so, we can skip this step
+        # and maybe save a little bit of time, especially if we need to
+        # process many points.
         x, y = coordinates
 
+        # Remove concentric, multiples of 360 degrees
         norm_degs_x = x - (int(x / 360) * 360)
 
+        # Calculate geographic longitude based on hemisphere
         if norm_degs_x > 180: norm_degs_x = norm_degs_x - 360
         if norm_degs_x < -180: norm_degs_x = norm_degs_x + 360
 
+        # Again, remove concentric, multiples of 360 degrees
         norm_degs_y = y - (int(y / 360) * 360)
         
+        # Calculate geographic latitude based on quadrant
         if 90< norm_degs_y <= 270: norm_degs_y = 180 - norm_degs_y
         if 270 < norm_degs_y <= 360: norm_degs_y = norm_degs_y - 360
 
--- src/validators/validate_coordinates.py
+++ src/validators/validate_coordinates.py
@@ -1,5 +1,9 @@
 
-def coordinates_are_valid(coordinates, srid=4326):
+def coordinates_are_valid(coordinates):
+    """Simple validation function.  Returns True if both x and y values
+    of the coordinate pair are within acceptable ranges of geographic
+    latitude and longitude"""
+
     x, y = coordinates
     x_is_valid = -180 <= x <= 180
     y_is_valid = -90 <= y <= 90
